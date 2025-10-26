dad7760912ad408f7e3decd95c9a365694d488ae
Shreyansh K <shreyansh.kashyap@outlook.in>
2025-10-25 23:42:59 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDKgqMN2YiswpsrAWawodhuLF27aoB3p64",
+    authDomain: "meme-51314.firebaseapp.com",
+    databaseURL: "https://meme-51314-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "meme-51314",
+    storageBucket: "meme-51314.firebasestorage.app",
+    messagingSenderId: "604768808089",
+    appId: "1:604768808089:web:26134d5a0a0c92c6065c08"
+};
+
+// GIPHY API Key
+const GIPHY_API_KEY = "coVc1IHu97MvJb0pN3BzBXqN3uWiLLwA";
